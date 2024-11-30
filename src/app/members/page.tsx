9c8bf00d3e7f3aed767522a21cf5c4b88e1183de
Kirill Zlobin <jsdevzk@gmail.com>
2024-11-30 22:46:57 +0100

    Initial commit

---
+++ src/app/members/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const MembersPage = () => {
+  return (
+    <div>
+      <h3 className="text-3xl">Members page</h3>
+      <Link href="/">Back to home page</Link>
+    </div>
+  );
+};
+
+export default MembersPage;
