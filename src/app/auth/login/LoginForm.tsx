4ad2ad264992afd0de42c2608150831513cf1a04
Kirill Zlobin <jsdevzk@gmail.com>
2024-12-01 11:42:56 +0100

    Added validation with zod for React Hook Form

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import { loginSchema, LoginSchema } from "@/app/lib/schemas/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { GiPadlock } from "react-icons/gi";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = (data: LoginSchema) => {
+    console.log(data);
+  };
+
   return (
     <Card className="w-2/5 mx-auto">
       <CardHeader className="flex flex-col items-center justify-center">
@@ -15,11 +33,11 @@ const LoginForm = () => {
         </div>
       </CardHeader>
       <CardBody>
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-4">
-            <Input label="Email" variant="bordered" />
-            <Input label="Password" type="password" variant="bordered" />
-            <Button fullWidth color="secondary" type="submit">
+            <Input label="Email" variant="bordered" defaultValue="" {...register("email")} isInvalid={!!errors.email} errorMessage={errors.email?.message} />
+            <Input label="Password" type="password" variant="bordered" defaultValue="" {...register("password")} isInvalid={!!errors.password} errorMessage={errors.password?.message} />
+            <Button isDisabled={!isValid} fullWidth color="secondary" type="submit">
               Login
             </Button>
           </div>
