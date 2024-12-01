3183d47dcae56ec75f3fab093134fe1f17f9f53a
Kirill Zlobin <jsdevzk@gmail.com>
2024-12-01 23:06:59 +0100

    Tested sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
+import { auth, signOut } from "@/auth";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button as={Link} href="/members">
-        Click me
-      </Button>
+      <h3 className="text-2xl font-semibold">User session data:</h3>
+      {session ? (
+        <div>
+          <pre>{JSON.stringify(session, null, 2000)}</pre>
+          <form
+            action={async () => {
+              "use server";
+
+              await signOut();
+            }}
+          >
+            <Button type="submit" color="primary" variant="bordered">
+              Sign out
+            </Button>
+          </form>
+        </div>
+      ) : (
+        "Not signed in"
+      )}
     </div>
   );
 }
