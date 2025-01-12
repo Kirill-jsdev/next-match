30c6604480f86dfa75534498b3d7fe51954203ab
Kirill Zlobin <jsdevzk@gmail.com>
2025-01-12 20:07:53 +0100

    Added page templates for PhotosPage and ChatPage

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth";
 import { prisma } from "../lib/prisma";
+import { Photo } from "@prisma/client";
 
 export async function getMembers() {
   const session = await auth();
@@ -22,6 +23,23 @@ export async function getMembers() {
 export async function getMemberByUserId(userId: string) {
   try {
     return await prisma.member.findUnique({ where: { userId } });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getMemberPhotosByUserId(userId: string) {
+  try {
+    const member = await prisma.member.findUnique({
+      where: { userId },
+      select: { photos: true }, //this will only return the photos
+    });
+
+    if (!member) {
+      return null;
+    }
+
+    return member.photos.map((p) => p) as Photo[];
   } catch (error) {
     console.error(error);
   }
--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -1,4 +1,5 @@
 import { getMemberByUserId } from "@/app/actions/memberActions";
+import { CardBody, CardHeader, Divider } from "@nextui-org/react";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -13,7 +14,13 @@ const MemberDetailsPage = async ({ params }: { params: { userId: string } }) => 
     return notFound();
   }
 
-  return <div>{member?.country}</div>;
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-secondary">Profile</CardHeader>
+      <Divider />
+      <CardBody>{member.description}</CardBody>
+    </>
+  );
 };
 
 export default MemberDetailsPage;
